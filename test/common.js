f1b6df801fc260d2f46313136c3e2529c007e491
Sune Simonsen <sune@we-knowhow.dk>
2016-04-01 21:44:23 +0200

    Upgraded unexpected dev dependency and unexpected-documentation-site-generator

--- test/common.js
+++
@@ -1,6 +0,0 @@
-/*global unexpected:true, moment:true*/
-moment = require('moment-timezone');
-moment.tz.setDefault('Europe/Copenhagen');
-
-unexpected = require('unexpected').clone();
-unexpected.use(require('../lib/unexpected-moment'));
